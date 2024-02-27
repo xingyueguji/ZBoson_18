e93b57d786953b50533ea30881ddd540352d8e9b
Zheng Huang <zhuang81@uic.edu>
2024-02-27 13:14:26 -0600

    Add Gen Z mass calculation Change path to local Now the bin number is 120 Reco has tau cut

--- MC_18.h
+++ MC_18.h
@@ -9,9 +9,12 @@ class MC_18{
 	void SetupBranches();
 	bool SetupCut();
 	void Plot_and_Pull(TH1D* hist, TH1D* hist2, TCanvas* c1, TPad* p1, TPad* p2);
+	void Plot_and_Pull_gen(TH1D* hist, TH1D* hist2, TCanvas* c1, TPad* p1, TPad* p2);
+    void Calc_Z_gen(TH1D* h1);
 	
 
-	TString MC_File_Path = "/eos/cms/store/group/phys_heavyions/abaty/Zmumu_Samples_MC/VertexCompositeTree_DYJetsToLL_TuneCP5_HydjetDrumMB_Pythia8_HINPbPbAutumn18_DiMuMC_20190808.root";
+	//TString MC_File_Path = "/eos/cms/store/group/phys_heavyions/abaty/Zmumu_Samples_MC/VertexCompositeTree_DYJetsToLL_TuneCP5_HydjetDrumMB_Pythia8_HINPbPbAutumn18_DiMuMC_20190808.root";
+	TString MC_File_Path = "./VertexCompositeTree_DYJetsToLL_TuneCP5_HydjetDrumMB_Pythia8_HINPbPbAutumn18_DiMuMC_20190808.root";
 	TFile *f1;
 	TDirectoryFile *dir1;
 	TTree *t1;
@@ -34,6 +37,18 @@ class MC_18{
 	Float_t PhiD2[5000] = {};
 	Short_t chargeD2[5000] = {};
 	vector<float> *weightLHE_gen = 0;
+
+	//These are I believe gen level
+	UInt_t candSize_gen = 0;
+	Float_t weight_gen = 0;
+	Short_t DecayID_gen[5000] = {};
+	Float_t pTD1_gen[5000] = {};
+	Float_t EtaD1_gen[5000] = {};
+	Float_t PhiD1_gen[5000] = {};
+	Float_t pTD2_gen[5000] = {};
+	Float_t EtaD2_gen[5000] = {};
+	Float_t PhiD2_gen[5000] = {};
+
 
 	Int_t nentries = 0;
 
@@ -67,7 +82,6 @@ void MC_18::SetupRootfile(){
 		cout << "TTree VertexCompositeNtuple;1 cannot be opened! " << endl;
 	}
 }
-
 void MC_18::SetupBranches(){
 	//Enable required branches
 	t1->SetBranchStatus("*",0);
@@ -87,6 +101,15 @@ void MC_18::SetupBranches(){
 	t1->SetBranchStatus("PhiD2",1);
 	t1->SetBranchStatus("chargeD2",1);
 	t1->SetBranchStatus("weightLHE_gen",1);
+	t1->SetBranchStatus("candSize_gen",1);
+	t1->SetBranchStatus("weight_gen",1);
+	t1->SetBranchStatus("DecayID_gen",1);
+	t1->SetBranchStatus("pTD1_gen",1);
+	t1->SetBranchStatus("EtaD1_gen",1);
+	t1->SetBranchStatus("PhiD1_gen",1);
+	t1->SetBranchStatus("pTD2_gen",1);
+	t1->SetBranchStatus("EtaD2_gen",1);
+	t1->SetBranchStatus("PhiD2_gen",1);
 	//Link to variables
 	t1->SetBranchAddress("candSize",&candSize);
 	t1->SetBranchAddress("centrality",&centrality);
@@ -104,6 +127,15 @@ void MC_18::SetupBranches(){
 	t1->SetBranchAddress("PhiD2",PhiD2);
 	t1->SetBranchAddress("chargeD2",chargeD2);
 	t1->SetBranchAddress("weightLHE_gen",&weightLHE_gen);
+	t1->SetBranchAddress("candSize_gen",&candSize_gen);
+	t1->SetBranchAddress("weight_gen",&weight_gen);
+	t1->SetBranchAddress("DecayID_gen",&DecayID_gen);
+	t1->SetBranchAddress("pTD1_gen",&pTD1_gen);
+	t1->SetBranchAddress("EtaD1_gen",&EtaD1_gen);
+	t1->SetBranchAddress("PhiD1_gen",&PhiD1_gen);
+	t1->SetBranchAddress("pTD2_gen",&pTD2_gen);
+	t1->SetBranchAddress("EtaD2_gen",&EtaD2_gen);
+	t1->SetBranchAddress("PhiD2_gen",&PhiD2_gen);
 }
 bool MC_18::SetupCut(){
 	int isPassed = 0;
@@ -144,7 +176,7 @@ void MC_18::Plot_and_Pull(TH1D* hist, TH1D* hist2, TCanvas* c1, TPad* p1, TPad* 
 	text->SetNDC();
 	text->DrawLatex(0.2,0.8,Form("# of entries is %i",int(integral)));
 	p2->cd();
-	for (int i=1; i<=60; i++){
+	for (int i=1; i<=numBins; i++){
 		double bincenter = hist->GetXaxis()->GetBinCenter(i);
 		double fitresult = fit->Eval(bincenter);
 		double binerror = hist->GetBinError(i);
@@ -166,4 +198,72 @@ void MC_18::Plot_and_Pull(TH1D* hist, TH1D* hist2, TCanvas* c1, TPad* p1, TPad* 
 	hist2->GetYaxis()->CenterTitle();
 	hist2->Draw("P HIST E1");
 
+}
+void MC_18::Plot_and_Pull_gen(TH1D* hist, TH1D* hist2, TCanvas* c1, TPad* p1, TPad* p2){
+	gStyle->SetOptFit(1);
+	hist2->GetXaxis()->SetTitle("Invariant Mass(GeV)");
+	c1->cd();
+	//TF1 *fit = new TF1("voigt","[0]*TMath::Voigt(x-[1]-91.1876,[2],[3]+2.4955)",60,120);
+	TF1 *fit = new TF1("BreitWigner","[0]*TMath::BreitWigner(x, [1]+91.1876, [2]+2.4955)",60,120);
+	int numBins = hist->GetNbinsX();
+	int maxBin = hist->GetMaximumBin();
+	fit->SetParameter(0,20000);
+	//fit->SetParameter(1,h_m_1->GetMean()-91.1876);
+	fit->SetParameter(1,0);
+	fit->SetLineWidth(1);
+	fit->SetParameter(2,0);
+	p1->SetMargin(0.1,0.1,0,0.1);
+	p2->SetMargin(0.1,0.1,0.2,0);
+	p1->Draw();
+	p2->Draw();
+	p1->cd();
+	//p1->SetLogy();
+	hist->Fit(fit,"LQR");
+	hist->Draw();
+	Double_t integral = hist->Integral(1,numBins);
+	TLatex *text = new TLatex();
+	text->SetNDC();
+	text->DrawLatex(0.2,0.8,Form("# of entries is %i",int(integral)));
+	p2->cd();
+	for (int i=1; i<=numBins; i++){
+		double bincenter = hist->GetXaxis()->GetBinCenter(i);
+		double fitresult = fit->Eval(bincenter);
+		double binerror = hist->GetBinError(i);
+		double bincontent = hist->GetBinContent(i);
+		double pullvalue = (bincontent - fitresult) / binerror;
+		hist2->SetBinContent(i,pullvalue);
+		hist2->SetBinError(i,0.5);
+	}
+	hist2->GetYaxis()->SetLabelSize(0.08);
+	hist2->GetXaxis()->SetLabelSize(0.1);
+	hist2->GetXaxis()->SetTitleSize(0.1);
+	hist2->SetStats(0);
+	hist2->SetTitle("");
+	hist2->GetYaxis()->SetTitle("Pull");
+	hist2->GetYaxis()->SetTitleSize(0.15);
+	hist2->GetYaxis()->SetTitleOffset(0.2);
+	hist2->GetXaxis()->SetTitleOffset(0.9);
+	hist2->GetXaxis()->SetTitle("Invariant Mass(GeV)");
+	hist2->GetYaxis()->CenterTitle();
+	hist2->Draw("P HIST E1");
+
+}
+void MC_18::Calc_Z_gen(TH1D* h1){
+	for (int i=0; i<this->candSize_gen; i++){
+		TLorentzVector muon1;
+		Float_t pt_muon_1 = this->pTD1_gen[i];
+		Float_t pt_muon_2 = this->pTD2_gen[i];
+		Float_t eta_muon_1 = this->EtaD1_gen[i];
+		Float_t eta_muon_2 = this->EtaD2_gen[i];
+		if (pt_muon_1 < 20 || pt_muon_2 < 20 || abs(eta_muon_1) > 2.4 || abs(eta_muon_2) > 2.4) continue;
+		Float_t phi_muon_1 = this->PhiD1_gen[i];
+		Float_t phi_muon_2 = this->PhiD2_gen[i];
+		muon1.SetPtEtaPhiM(pt_muon_1,eta_muon_1,phi_muon_1,0.105);
+		TLorentzVector muon2;
+		muon2.SetPtEtaPhiM(pt_muon_2,eta_muon_2,phi_muon_2,0.105);
+		TLorentzVector ZBoson = muon1 + muon2;
+		Float_t M = ZBoson.M();
+		if (M < 60 || M >= 120) continue;
+		h1->Fill(M,this->weight_gen/10000);
+	}
 }
--- MC_plot.C
+++ MC_plot.C
@@ -10,47 +10,44 @@ void MC_plot(bool isCut = 1){
 	bool eventnotpassed = 0; // 1 means not passed, 0 means passed cut
 
 	//Histogram
-	TH1D *mc_reco_Zmass = new TH1D("mc_reco_Zmass","mc_reco_Zmass",60,60,120);
-	TH1D *h_p_1 = new TH1D("h_p_1","Pull",60,60,120);
+	TH1D *mc_reco_Zmass = new TH1D("mc_reco_Zmass","mc_reco_Zmass",120,60,120);
+	TH1D *h_p_1 = new TH1D("h_p_1","Pull",120,60,120);
+	TH1D *mc_gen_Zmass = new TH1D("mc_gen_Zmass","mc_gen_Zmass",120,60,120);
+	TH1D *h_p_2 = new TH1D("h_p_2","Pull",120,60,120);
 
 	//Canvas
 	TCanvas *c1 = new TCanvas("","",800,600);
+	TCanvas *c2 = new TCanvas("","",800,600);
 
 	//TPad
 	TPad *p_1 = new TPad("p_1","Large",0,0.3,1,1);
 	TPad *p_2 = new TPad("p_2","Small",0,0,1,0.3);
+	TPad *p_3 = new TPad("p_3","Large",0,0.3,1,1);
+	TPad *p_4 = new TPad("p_4","Small",0,0,1,0.3);
 
 	//Event loop
-
 	int nentries = obj1->t1->GetEntries();
 	cout << "We have " << nentries << " events in total" << endl;
 
-	//ofstream out1("output.txt",std::ios::trunc);
-
 	for (int i=0; i<nentries; i++){
-		 //out1.seekp(0);
-		 //out1 << "Updated content for iteration " << i << std::endl;
-		if (i%1 == 0) cout << "Event " << i << endl;
+		if (i%100000 == 0) cout << "Event " << i << endl;
 		obj1->t1->GetEntry(i);
-		cout << "TEST3" << endl;
 		eventnotpassed = obj1->SetupCut();
 		if (isCut && eventnotpassed) continue; // using cut and not passed, skip event
 
 		for (int j=0;j<obj1->candSize; j++){
-			cout << "TEST" << endl;
-			if (obj1->mass[j] < 60 || obj1->mass[j] >= 120 ) continue;
+			if (obj1->mass[j] < 60 || obj1->mass[j] >= 120 || obj1->DecayID_gen[j] == 15) continue; 
 			mc_reco_Zmass->Fill(obj1->mass[j],obj1->weightLHE_gen->at(1080)/10000.0);
 		}
-
-		//out1.flush();
-		cout << "TEST2" << endl;
-		
+		obj1->Calc_Z_gen(mc_gen_Zmass);
 	}
 
 
 	//Draw plot
 	obj1->Plot_and_Pull(mc_reco_Zmass,h_p_1,c1,p_1,p_2);
+	obj1->Plot_and_Pull_gen(mc_gen_Zmass,h_p_2,c2,p_3,p_4);
 	c1->SaveAs("./mcplot/mc_reco_Zmass.pdf");
+	c2->SaveAs("./mcplot/mc_gen_Zmass.pdf");
 
 
 }
